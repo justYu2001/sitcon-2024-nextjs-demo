86654aaf2ac8e23a35eff549a45d6668b15eacec
justYu2001 <jack4781012@gmail.com>
2024-02-12 21:22:41 +0900

    build: add sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fd8c5d808cb78c19b665c0a72099d8b4@o4505503191793664.ingest.sentry.io/4506731528912896",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
